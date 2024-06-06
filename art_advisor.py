a4ca1b2876e9f7c824ac015b0625a1488e38574b
simoncwang <wang.c.simon@gmail.com>
2024-06-06 14:30:42 -0400

    added requirements

--- art_advisor.py
+++ art_advisor.py
@@ -2,17 +2,17 @@ import base64
 import requests
 from openai import OpenAI
 import gradio as gr
-import numpy as np 
+import numpy as np
 from PIL import Image as im 
 
-def critic(np_image):
+def critic(your_image, openai_api_key):
 
     # converting input image from numpy array to png and saving it
-    image = im.fromarray(np_image)
-    image.save('test_images/image.png')
+    image = im.fromarray(your_image)
+    image.save('images/image.png')
 
     # OpenAI API Key
-    api_key = "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"
+    # api_key = "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"
 
     # Function to encode the image
     def encode_image(image_path):
@@ -20,14 +20,14 @@ def critic(np_image):
             return base64.b64encode(image_file.read()).decode('utf-8')
 
     # # Path to your image
-    image_path = "test_images/image.png"
+    image_path = "images/image.png"
 
     # Getting the base64 string
     base64_image = encode_image(image_path)
 
     headers = {
     "Content-Type": "application/json",
-    "Authorization": f"Bearer {api_key}"
+    "Authorization": f"Bearer {openai_api_key}"
     }
 
     payload = {
@@ -63,10 +63,10 @@ def critic(np_image):
 
 demo = gr.Interface(
     critic,
-    gr.Image(),
+    inputs=[gr.Image(), "text"],
     outputs=["text"],
     title = "Art Advisor",
-    description = "Welcome to your personal art advisor! I can help give you constructive feedback so you can take your art to the next level!",
+    description = "Welcome to your personal art advisor! I can give you constructive feedback to help you learn and take your art to the next level!",
 )
 
 demo.launch()

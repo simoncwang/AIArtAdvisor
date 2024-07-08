f0874d83d731750180916b86bbf300a54f6206a8
simoncwang <wang.c.simon@gmail.com>
2024-07-08 17:51:46 -0400

    added skill level selector

--- art_advisor.py
+++ art_advisor.py
@@ -7,7 +7,9 @@ import os
 from PIL import Image as im 
 
 # art critique function
-def critique(your_image, openai_api_key, user_text):
+def critique(language, system_role, your_image, openai_api_key, user_text):
+
+    print(system_role)
 
     # converting input image from numpy array to png and saving it
     image = im.fromarray(your_image)
@@ -37,10 +39,12 @@ def critique(your_image, openai_api_key, user_text):
     # the defualt input text prompt
     default_text = "Please give me constructive feedback on this artwork, and things I could work on to improve it."
 
+    # "You are a knowledgeable art critic, skilled in giving the artist feedback to help them improve their artwork. Please list out both positive and negative aspects of their work in a constructive way. Also, do your best to take into account any specific questions or comments they have and answer them!"
+
     payload = {
     "model": "gpt-4o",
     "messages": [
-        {"role": "system", "content": "You are a knowledgeable art critic, skilled in giving the artist feedback to help them improve their artwork. Please list out both positive and negative aspects of their work in a constructive way. Also, do your best to take into account any specific questions or comments they have and answer them!"},
+        {"role": "system", "content": system_role + language},
         {
         "role": "user",
         "content": [
@@ -72,6 +76,24 @@ def critique(your_image, openai_api_key, user_text):
 
 
 # Gradio demo!
+
+# the role of the system
+system_prompt = "test"
+
+# system role prompt strings
+genz_prompt = "Give constructive feedback for this painting including negative and positive aspects as well as how to improve, but use gen-z slang and references in your response."
+
+beginner_prompt = "You are an art teacher focused on inspiring young or beginner artists. Please list out both positive and negative aspects of their work in a constructive way. Answer their questions and give advice using very simple terms that are easy to understand for a beginner artist or young child, but still accurate enough to teach them good art principles! Think children's book or cartoon tone!"
+
+intermediate_prompt = "You are an art critic that focuses on intermediate level artists. Think a high school student or artist that has a few years of experience! Give them constructive feedback by listing out both positive and negative aspects of their work as well as how to improve it!"
+
+advanced_prompt = "You are an expert art critic that specializes in helping professional artists improve. You are skilled and extremely knowledgeable about advanced art techniques. Please list out both positive and negative aspects of their work in a constructive way. Answer any questions the artist might have and provide feedback using a tone that an extremely experienced artist would benefit from. Think art textbook or professional artist in terms of the tone of your response!"
+
+# language prompts
+english_prompt = "Please write your response in English."
+
+chinese_prompt = "Please write your response in Chinese."
+    
 with gr.Blocks() as demo:
     gr.Markdown(
         """
@@ -102,12 +124,30 @@ with gr.Blocks() as demo:
             image = gr.Image()
             api_key = gr.Textbox(label="OpenAI API Key", placeholder="your API key here")
             user_text = gr.Textbox(label="Specific questions", placeholder="your questions or comments here")
+
+            skill_select = gr.Dropdown(
+                choices=[("Gen-Z", genz_prompt), ("Beginner", beginner_prompt), ("Intermediate", intermediate_prompt), ("Advanced", advanced_prompt)], 
+                label="Level", 
+                info="Select your desired skill level to cater your feedback!",
+                value=intermediate_prompt,
+                interactive=True
+            )
+
+            language_select = gr.Dropdown(
+                choices=[("English", english_prompt), ("Chinese", chinese_prompt)], 
+                label="Language", 
+                value=english_prompt,
+                interactive=True
+            )
+
             submit_btn = gr.Button("Submit")
+
+
         with gr.Column():
             gr.Markdown("## Feedback")
             result = gr.Markdown(label="Feedback")
 
-    submit_btn.click(critique, inputs=[image,api_key,user_text], outputs=result)
+    submit_btn.click(critique, inputs=[language_select,skill_select,image,api_key,user_text], outputs=result)
 
 # demo = gr.Interface(
 #     critique,
